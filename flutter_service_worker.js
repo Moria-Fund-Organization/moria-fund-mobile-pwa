36522461207c1126e19abb95df9d40aa0d8666cb
Akil <fadhli.tech@gmail.com>
2026-06-27 06:23:37 +0700

    fix some bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ce1b3f2819edeb9eb45a551f67d0d67c",
+const RESOURCES = {"flutter_bootstrap.js": "41cd8482a1b8446bb45b9769e7004fa9",
 "version.json": "6e1bbc7c77e0d799c65640cf442bd079",
 "index.html": "dc7ff1d9c27318dfbe4daa0857f2a5e1",
 "/": "dc7ff1d9c27318dfbe4daa0857f2a5e1",
-"main.dart.js": "20a726ead9a4d888c68590531a3ecd34",
+"main.dart.js": "fa6d22adb9e43f80f0f7700243a51826",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
