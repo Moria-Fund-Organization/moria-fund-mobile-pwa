7ddbfb81feca7bc32230725526e37b0172bfec6f
Akil <fadhli.tech@gmail.com>
2026-07-31 13:58:38 +0700

    build: publish mobile PWA update (prod flavor)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1965285206"
+    serviceWorkerVersion: "2504424712"
   }
 });
